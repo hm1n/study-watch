c515cb063b92cc3a2d9c79927b623b961c0e506a
shinhm1 <shinhm1@naver.com>
2026-03-03 17:42:13 +0900

    fix: 분 표기 수정

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,7 +1,7 @@
 export function formatTime(ms) {
   const centiseconds = Math.floor(ms / 10) % 100;
   const seconds = Math.floor(ms / 1000) % 60;
-  const minutes = Math.floor(ms / 60000);
+  const minutes = Math.floor(ms / 60000) % 60;
   const hours = Math.floor(ms / 3600000);
 
   const centiStr = String(centiseconds).padStart(2, '0');
